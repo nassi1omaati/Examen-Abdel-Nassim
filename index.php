acde9fab8c5b03af0afe38d6509ff94e0276935f
Abdel Souhali <90617360+waterguy1010@users.noreply.github.com>
2023-10-30 13:01:12 +0100

    birb

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ include "dbconnect.php"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vapor FC</title>
     <link rel="stylesheet" href="styles.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </head>
 
@@ -30,12 +31,12 @@ include "dbconnect.php"
     </header>
 
     <div class="flex-container">
-        <div><img src="img/kicks1.jpg" alt="swag" width="500" height="600"></div>
-        <div><img src="img/kicks2.jpg" alt="swag" width="500" height="600"></div>
-        <div><img src="img/kicks3.jpg" alt="swag" width="500" height="600"></div>
-        <div> <img src="img/kicks4.jpg" alt="swag" width="500" height="600"></div>
-        <div> <img src="img/kicks5.jpg" alt="swag" width="500" height="600"></div>
-        <div><img src="img/nutter.jpg" alt="swag" width="500" height="600"></div>
+        <div><img src="img/kicks1.jpg"class="img-fluid" alt="swag" width="500" height="600"></div>
+        <div><img src="img/kicks2.jpg" class="img-fluid"alt="swag" width="500" height="600"></div>
+        <div><img src="img/kicks3.jpg" class="img-fluid"alt="swag" width="500" height="600"></div>
+        <div> <img src="img/kicks4.jpg"class="img-fluid" alt="swag" width="500" height="600"></div>
+        <div> <img src="img/kicks5.jpg" class="img-fluid" alt="swag" width="500" height="600"></div>
+        <div><img src="img/nutter.jpg" class="img-fluid" alt="swag" width="500" height="600"></div>
     </div>
 
 
