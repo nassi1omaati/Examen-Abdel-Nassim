7c3c4a10e7ddca9926071b353aa486d2e1d20d1e
Nassim1Omaati <2151890@talnet.nl>
2023-11-02 07:54:10 +0100

    de Homepage gedeeltelijk afgerond alleen nog interactief

--- index.php
+++ index.php
@@ -9,14 +9,16 @@ include "dbconnect.php"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vapor FC</title>
+    <title>Kiqqs</title>
     <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    <div class="header">
+
     <div class="container">
     <div class="navbar">
         <div class="logo1">
@@ -30,17 +32,21 @@ include "dbconnect.php"
             <li><a class="category" href="#">Contact</a></li>
             </ul>
         </nav>
+        <img src="img/cart.png" width="30px" height="30px">
      </div>
      <div class="row">
         <div class="col-2">
-           <h1>Voetbal op nieuwe wijze!</h1> 
-           <P>lorem ipsum</P>
+           <h1>Voetbal van de grond af! <br>een nieuwe wereld!</h1> 
+           <P>What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry.</P>
+           <a href=""class="button">bekijk collectie</a>
         </div>
         <div class="col-2"></div>
-            <img src="img/crissyboy.png" alt="">
+            <img src="img/crissyboy.png">
+        </div>
+     </div>
     </div>
-</div>
-    </div>
+ </div>
+
 
 
 </body>
