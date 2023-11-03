83221883ffb1391f1ecad912fbc4a1fc75776113
Nassim1Omaati <2151890@talnet.nl>
2023-11-03 17:23:37 +0100

    footer netter gemaakt en meer geoordend.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ include "dbconnect.php"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet">
+    
 </head>
 
 <body>
@@ -27,9 +28,9 @@ include "dbconnect.php"
         <nav>
             <ul>
             <li><a class="category" href="producten.php">Producten</a></li>
-            <li><a class="category" href="#">over ons</a></li>
-            <li><a class="category" href="#">contact</a></li>
-            <li><a class="category" href="#">account</a></li>
+            <li><a class="category" href="#">Over ons</a></li>
+            <li><a class="category" href="#">Contact</a></li>
+            <li><a class="category" href="#">Account</a></li>
             </ul>
         </nav>
         <img src="img/cart.png" width="30px" height="30px">
@@ -49,11 +50,29 @@ include "dbconnect.php"
  </div>
 
 <!-- hier beginnen we aan de footer -->
-
 <div class="footer">
-  <a href="index.php"> <img src="img/Logo1.png" width="250"></a>
+    <div class="container">
+        <div class="row">
+<div class="footerline">
+    <hr>
+</div>  
+    <div class="footer1">
+        <h1>Links</h1>
 
-
+    <div class="footer2">
+    <h1>locatie</h1>
+    <ul>
+            <li>Osdorpplein</li>
+            <li>0612345678</li>
+            <li>postcode 1012AB</li>
+            <li>Amsterdam</li>
+    </ul>
+        </div>
+        <div class="footer3">
+        <a href="index.php"> <img src="img/Logo1.png" width="150"></a>
+        </div>
+    </div>
+ 
 </body>
 
    
