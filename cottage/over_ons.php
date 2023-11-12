0300a10281cc0799888a8fcd4c381badfd5ce1cb
Nassim1Omaati <2151890@talnet.nl>
2023-11-12 17:12:46 +0100

    Over ons tekst toegevoegd

--- cottage/over_ons.php
+++ cottage/over_ons.php
@@ -28,9 +28,16 @@ include "header.php";
     <div class="over">
     <img src="images/over_ons.png" class="foto">
       <div class="text">
-        <h2>Over ons</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. 
-             Ullam dolorem repudiandae cumque voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
+        <h1>Ontdek de Kiqqs-ervaring</h1>
+
+          <p>In 2023 zag "Kiqqs" het levenslicht, opgericht door een gedreven groep ingenieurs en sportenthousiastelingen met één doel: 
+            het creëren van cutting-edge voetbalvelden. 
+            De naam, een fusie van "Kick" en "Quality," weerspiegelt het streven naar innovatie en topsportprestaties.
+            Met geavanceerde technologie voor grondanalyse en aanpassing, streeft Kiqqs naar duurzaamheid en voldoet aan de strengste professionele normen. 
+            <br>
+            <br>
+            Milieuvriendelijkheid staat centraal, met gebruik van gerecyclede materialen en vooruitstrevende waterbeheeroplossingen. 
+            In 2023 heeft Kiqqs snel erkenning verworven met wereldwijde projecten, waarmee het een opkomend merk is in de sportinfrastructuur, gedreven door passie en toewijding.</p>
         <div class="data">
         <a href="contact.php" class="button_over">Contacteren</a>
         </div>
