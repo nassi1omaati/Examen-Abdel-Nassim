dea6d1cd509f0808c522a44677215b6ac3c38bee
Nassim1Omaati <2151890@talnet.nl>
2023-11-13 10:06:20 +0100

    menu aangepast.

--- cottage/menu-1.php
+++ cottage/menu-1.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-light" style="background-color: #fff;">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php"><img src="images/Logo1.png" width="150"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="service.php">service</a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="service.php">Service</a></li>
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="over_ons.php">Over ons</a></li>
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="contact.php">Contact</a></li>
 
