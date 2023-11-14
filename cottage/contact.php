b1a1ff498049c259f6e282ad517c5a3169d9c131
Nassim1Omaati <2151890@talnet.nl>
2023-11-14 11:57:03 +0100

    Contact aangepast.

--- cottage/contact.php
+++ cottage/contact.php
@@ -15,10 +15,6 @@ include "header.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kiqqs</title>
     <link rel="stylesheet" href="style.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet">  -->
-
 </head>
 
 <body>
@@ -48,7 +44,7 @@ include "header.php";
                     <label for="subject">Onderwerp</label>
                     <textarea id="subject" name="subject" placeholder="..." style="height:100px"></textarea>
 
-                    <input type="submit" value="Submit">
+                    <input type="submit" value="Verzenden">
                 </form>
             </div>
 
